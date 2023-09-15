616ba1e2fb3c2e6bcf0f493ebc6a221008b8b9ed
yuval leibovich <yoval1012@gmail.com>
2023-09-15 07:41:43 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+# Connect to your MongoDB server (change the connection URI as needed)
+client = MongoClient('mongodb://localhost:27017')
+db = client['animals_db']  # Replace 'your_database_name' with your database name
 
 # Sample initial list of animals as dictionaries
 animals = [{'name': 'Cat', 'description': 'A small domesticated carnivorous mammal.', 'url': 'https://en.wikipedia.org/wiki/Cat'},
@@ -9,7 +14,9 @@ animals = [{'name': 'Cat', 'description': 'A small domesticated carnivorous mamm
 
 @app.route('/')
 def index():
-    return render_template('index.html', animals=animals)
+    # Retrieve the list of animals from MongoDB
+    animals_from_db = list(db.animals.find())
+    return render_template('index.html', animals=animals_from_db)
 
 @app.route('/add_animal', methods=['POST'])
 def add_animal():
@@ -18,17 +25,23 @@ def add_animal():
     new_animal_url = request.form.get('new_url')
     if new_animal_name and new_animal_description and new_animal_url:
         new_animal = {'name': new_animal_name, 'description': new_animal_description, 'url': new_animal_url}
-        animals.append(new_animal)
-    return jsonify({'animals': animals})
+        # Insert the new animal into MongoDB
+        db.animals.insert_one(new_animal)
+
+    # Retrieve the updated list of animals from MongoDB
+    animals_from_db = list(db.animals.find())
+    return jsonify({'animals': animals_from_db})
 
 @app.route('/remove_animal', methods=['POST'])
 def remove_animal():
     animal_name_to_remove = request.form.get('animal')
-    for animal in animals:
-        if animal['name'] == animal_name_to_remove:
-            animals.remove(animal)
-            break
-    return jsonify({'animals': animals})
+    # Remove the animal from MongoDB
+    db.animals.delete_one({'name': animal_name_to_remove})
+
+    # Retrieve the updated list of animals from MongoDB
+    animals_from_db = list(db.animals.find())
+    return jsonify({'animals': animals_from_db})
 
 if __name__ == '__main__':
-    app.run(port=3002)
+    app.run(port=3001)
+
