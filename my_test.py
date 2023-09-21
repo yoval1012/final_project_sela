441b1cd38c5ae97090c4aba81f7823ff1d9b880f
yuval leibovich <yoval1012@gmail.com>
2023-09-21 14:04:52 +0000

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -1,36 +1,27 @@
 import pytest
-from app import app
-from pytest_timeout import timeout
+from app import app, db
 
-@pytest.fixture
-def client():
+@pytest.fixture(scope='module')
+def test_client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-def test_index_route(client):
-    # Test the '/' route (index route)
-    rv = client.get('/')
-    assert rv.status_code == 200
-    assert b'Welcome to the Animal Catalog' in rv.data
+@pytest.fixture(scope='module')
+def test_db():
+    # Create a temporary test database
+    db.create_all()
 
-def test_add_animal_route(client):
-    # Test the '/add_animal' route
-    rv = client.get('/add_animal')
-    assert rv.status_code == 200
-    assert b'Add a New Animal' in rv.data
+    yield db
 
-def test_remove_animal_route(client):
-    # Test the '/remove_animal' route
-    rv = client.get('/remove_animal')
-    assert rv.status_code == 200
-    assert b'Remove an Animal' in rv.data
+    # Clean up the database after testing
+    db.drop_all()
 
-@timeout(10)  # Set the timeout to 10 seconds
-def test_my_function():
+def test_index_route(test_client):
     # Your test code here
-    print("Starting the test")
-    # Your test code here
-    print("Test completed")
+    pass
+
+# Other test functions
+
 
 
