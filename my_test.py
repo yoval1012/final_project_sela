97f5b7607edc77df05950f7a7987a95976324444
yuval leibovich <yoval1012@gmail.com>
2023-09-21 13:45:55 +0000

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -7,7 +7,23 @@ def client():
     with app.test_client() as client:
         yield client
 
-def test_index(client):
+def test_index_route(client):
+    # Test the '/' route (index route)
     rv = client.get('/')
     assert rv.status_code == 200
+    assert b'Welcome to the Animal Catalog' in rv.data
+
+def test_add_animal_route(client):
+    # Test the '/add_animal' route
+    rv = client.get('/add_animal')
+    assert rv.status_code == 200
+    assert b'Add a New Animal' in rv.data
+
+def test_remove_animal_route(client):
+    # Test the '/remove_animal' route
+    rv = client.get('/remove_animal')
+    assert rv.status_code == 200
+    assert b'Remove an Animal' in rv.data
+
+# Add more tests for specific routes and functionality as needed
 
