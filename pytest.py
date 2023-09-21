284e4a49aba81476cf38893bcb23e9fb2ac6e507
yuval leibovich <yoval1012@gmail.com>
2023-09-21 12:49:36 +0000

    Update test_syntax.py

---
+++ pytest.py
@@ -0,0 +1,33 @@
+import pytest
+from app import app
+
+
+# Initialize the Flask app for testing
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+
+# Test adding a new animal
+def test_add_animal(client, test_db):
+    # Define the data for the new animal
+    new_animal_data = {
+        'new_animal': 'Giraffe',
+        'new_description': 'A tall African mammal with a long neck.',
+        'new_url': 'https://en.wikipedia.org/wiki/Giraffe'
+    }
+
+    # Send a POST request to the /add_animal route with the new animal data
+    response = client.post('/add_animal', data=new_animal_data, follow_redirects=True)
+
+    # Check if the response status code is 200 (OK) after adding the animal
+    assert response.status_code == 200
+
+    # Check if the new animal exists in the test MongoDB database
+    added_animal = test_db.animals.find_one({'name': 'Giraffe'})
+    assert added_animal is not None
+    assert added_animal['name'] == 'Giraffe'
+    assert added_animal['description'] == 'A tall African mammal with a long neck.'
+    assert added_animal['url'] == 'https://en.wikipedia.org/wiki/Giraffe'
