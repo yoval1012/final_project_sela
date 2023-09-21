bc68ec6191db3c43f60027fd26ec14856ec2362b
yuval leibovich <yoval1012@gmail.com>
2023-09-21 14:12:35 +0000

    Update my_test.py

--- my_test.py
+++ my_test.py
@@ -1,27 +1,16 @@
 import pytest
-from app import app, db
-
-@pytest.fixture(scope='module')
-def test_client():
+from app import app
+@pytest.fixture
+def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
-@pytest.fixture(scope='module')
-def test_db():
-    # Create a temporary test database
-    db.create_all()
-
-    yield db
-
-    # Clean up the database after testing
-    db.drop_all()
-
-def test_index_route(test_client):
-    # Your test code here
-    pass
-
-# Other test functions
+        
+def test_index(client):
+    # Test if the response for the main route is 200 (OK)
+    rv = client.get('/')
+    assert rv.status_code == 200
 
 
 
